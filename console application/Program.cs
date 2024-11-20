9b2e6d9a1c145be0e0f305ff71d3e7c76eb3e2af
Josip1123 <jopetk@hotmail.com>
2024-11-20 10:49:35 +0100

    Test

---
+++ console application/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+Console.Write("Whats your name? " );
+var name = Console.ReadLine();
+if (string.IsNullOrEmpty(name))
+{
+    Console.WriteLine("Ooops!" +
+                      "");
+}
+Console.Write("Whats your e-mail? ");
+var email = Console.ReadLine();
+Console.WriteLine("Welcome " + name + " verification email has been sent to " + email );
+
